a0150a4eb14da80ab07f5b8a6e6ba6ac5cca109f
Muhammad Muttraf Rasool <muttrafgujjar.787@gmail.com>
2023-03-17 02:00:04 +0500

    Fix EsLint error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as allVar from './modules/Variables.js';
+import * as allVar from './modules/variables.js';
 
 const display = (data) => {
   data.forEach((i) => {
@@ -11,25 +11,25 @@ fetch(allVar.BASE_URL)
   .then((res) => res.json())
   .then((data) => display(data?.result));
 
-  allVar.Form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const userObj = {
-      user: e.target.user.value,
-      score: e.target.score.value,
-    };
-  
-    const postRequest = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userObj),
-    };
-  
-    await fetch(allVar.BASE_URL, postRequest).then((res) => res.json())
-      .then(() => window.location.reload());
-  });
+allVar.Form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const userObj = {
+    user: e.target.user.value,
+    score: e.target.score.value,
+  };
 
-  allVar.Refresh.addEventListener('click', () => {
-    window.location.reload();
-  });
+  const postRequest = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userObj),
+  };
+
+  await fetch(allVar.BASE_URL, postRequest).then((res) => res.json())
+    .then(() => window.location.reload());
+});
+
+allVar.Refresh.addEventListener('click', () => {
+  window.location.reload();
+});
